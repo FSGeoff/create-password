68bdf4e5c6f094ee69ba80d264ecc5ddd680c6e8
Geoff Nix <FullStackGeoff@gmail.com>
2020-09-17 10:22:17 -0400

    added all confirm statements to gather pwd info

--- script.js
+++ script.js
@@ -15,4 +15,12 @@ function generatePassword() {
 	var length = prompt(
 		"Choose the length of your password(8-128 characters)?"
 	);
+	var lCase = confirm(
+		"Would you like lower case characters in your password?"
+	);
+	var uCase = confirm(
+		"Would you like upper case characters in your password?"
+	);
+	var cNum = confirm("Would you like numbers in your password");
+	var sChars = confirm("would you like special characters in your password?");
 }
