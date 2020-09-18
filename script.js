f357793c4976a7f2232ad6d8cd58c0e1471dcfd1
Geoff Nix <FullStackGeoff@gmail.com>
2020-09-18 10:12:05 -0400

    made variable name more desriptive

--- script.js
+++ script.js
@@ -65,32 +65,32 @@ function generatePassword() {
 		";",
 		":",
 	];
-	var length = prompt(
+	var pwdLength = prompt(
 		"Choose the length of your password(8-128 characters)?"
 	);
-	var lCase = confirm(
+	var selectLCase = confirm(
 		"Would you like lower case characters in your password?"
 	);
-	if (lCase) {
+	if (selectLCase) {
 	}
-	var uCase = confirm(
+	var selectUCase = confirm(
 		"Would you like upper case characters in your password?"
 	);
-	if (uCase) {
+	if (selectUCase) {
 	}
-	var cNum = confirm("Would you like numbers in your password?");
-	if (cNum) {
+	var selectNum = confirm("Would you like numbers in your password?");
+	if (selectNum) {
 	}
-	var sChars = confirm("Would you like special characters in your password?");
-	if (sChars) {
+	var selectChars = confirm(
+		"Would you like special characters in your password?"
+	);
+	if (selectChars) {
 	}
 
 	// Checked to see that values were being passed
-	console.log(length);
-	console.log(lCase);
-	console.log(uCase);
-	console.log(cNum);
-	console.log(sChars);
-
-	document.getElementById("password").innerHTML = "Your Password is:";
+	console.log(pwdLength);
+	console.log(selectLCase);
+	console.log(selectUCase);
+	console.log(selectNum);
+	console.log(selectChars);
 }
